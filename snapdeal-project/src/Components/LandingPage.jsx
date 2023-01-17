333cf87185137704295dba96ddc30ff356be440f
BhaveshKumar <bk769221@gmail.com>
2023-01-17 13:38:16 +0530

    navbar-footer-landingPage

---
+++ snapdeal-project/src/Components/LandingPage.jsx
@@ -0,0 +1,6 @@
+
+export const LandingPage = () => {
+  return (
+    <div>LandingPage</div>
+  )
+}
---
+++ snapdeal-project/src/Components/Navbar.jsx
@@ -0,0 +1,8 @@
+
+export const Navbar = () => {
+  return (
+    <div>
+        <h1>Navbar</h1>
+    </div>
+  )
+}
