6b10b36f2be420bd270deb63458b41f964cc65f9
Bhairavnath Gotam <bhairavgotam066@gmail.com>
2023-01-20 01:23:03 +0530

    added day-3 task

---
+++ snapdeal-project/src/Components/B-Components/AdminPage.jsx
@@ -0,0 +1,45 @@
+import { Box, Button, FormControl, FormLabel, Heading, Input } from '@chakra-ui/react'
+import React from 'react'
+import { useState } from 'react'
+
+const AdminPage = () => {
+    const [data, setData] = useState({
+       name:"",
+       description:"",
+       price:0,
+       image:""
+    })
+    const [name, setName] = useState("")
+    const handleChange = (e)=>{
+          setName(e.target.value)
+          const {name, value} = e.target
+          setData({...data,[name]:value})
+    }
+    const handleSubmit = (e) =>{
+          e.preventDefault()
+        console.log("button data",data)
+        console.log("refresh prevented")
+        const arr=[]
+        arr.push(data)
+        localStorage.setItem("AdminData",JSON.stringify(arr))
+    }
+   
+  return (
+    <div>
+           <Heading>Admin Card</Heading>
+        <FormControl action=""   w={"500px"} margin={"auto"}>
+            <FormLabel htmlFor="">Enter Name of product</FormLabel>
+            <Input type="text" name='name' placeholder='Enter Name' onChange={handleChange}/>
+            <FormLabel htmlFor="">Decription</FormLabel>
+            <Input type="text" name="description" placeholder='Enter Decription' onChange={handleChange} />
+            <FormLabel htmlFor="" >Price</FormLabel>
+            <Input type="number"  name='price' placeholder='Enter Price' onChange={handleChange}/>
+            <FormLabel htmlFor="">Enter Image Url</FormLabel>
+            <Input type="text" name='image' placeholder='Enter Image url' onChange={handleChange}/>
+            <Button onClick={handleSubmit} colorScheme='red'>Add Products</Button>
+        </FormControl>
+    </div>
+  )
+}
+
+export default AdminPage
