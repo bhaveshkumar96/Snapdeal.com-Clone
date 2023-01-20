08eec5b23c18604c52fff0799e5ba8a0ddc5c27f
vaishnavi borkar <vaishnaviborkar29@gmail.com>
2023-01-20 11:57:03 +0530

    day-3

--- snapdeal-project/src/Components/Login/Login.jsx
+++ snapdeal-project/src/Components/Login/Login.jsx
@@ -1,23 +1,21 @@
 import React from "react";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { useLocation, useNavigate } from "react-router-dom";
+// import { useLocation, useNavigate } from "react-router-dom";
 
-import login from "../Redux/AuthReducer/action";
+import login from "../../Redux/action";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const location = useLocation();
+ 
   const dispatch = useDispatch();
-  const finalForm = location.state?.form?.pathname || "/";
-  const navigate = useNavigate();
-
+  
   const handleSubmit = (e) => {
     e.preventDefault();
     if (email && password) {
       dispatch(login({ email, password })).then((res) => {
-        navigate(finalForm, { replace: true });
+       
       });
     }
   };
