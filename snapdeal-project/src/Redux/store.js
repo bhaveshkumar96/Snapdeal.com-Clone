945aa23e26ea84540d2763c3c855c464fafb42f6
BhaveshKumar <bk769221@gmail.com>
2023-01-21 18:38:02 +0530

    day-5 final

--- snapdeal-project/src/Redux/store.js
+++ snapdeal-project/src/Redux/store.js
@@ -1,7 +1,3 @@
-import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
-import thunk from "redux-thunk";
-import { reducer as AdminReducer } from "./Admin/reducer";
-
-
+import {reducer as AdminReducer} from "../Redux/Admin/reducer"
 let rootReducer = combineReducers({AdminReducer})
 export const store = legacy_createStore(rootReducer,applyMiddleware(thunk));
