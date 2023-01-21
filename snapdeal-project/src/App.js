1510803805c704a0d8e45f6a6b6a9beecbae2db7
vaishnavi borkar <vaishnaviborkar29@gmail.com>
2023-01-21 18:34:23 +0530

    day-5

--- snapdeal-project/src/App.js
+++ snapdeal-project/src/App.js
@@ -1,5 +1,6 @@
 // import logo from "./logo.svg";
 import "./App.css";
+import Checkout from "./Components/Checkout/Checkout";
 import Login from "./Components/Login/Login";
 import SignUp from "./Components/Signup";
 import Allroutes from "./routes/Allroutes";
@@ -10,6 +11,7 @@ function App() {
       {/* <Login /> */}
       {/* <SignUp /> */}
       <Allroutes />
+      <Checkout />
     </div>
   );
 }
